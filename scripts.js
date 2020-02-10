fc771fdf40d13dd40b6c0fe57fb7af74966cfc97
Evie-Skinner18 <eskinner@spartaglobal.com>
2020-02-10 19:47:34 +0000

    Added a method to the Vue object and a v-model directive so we can dynamically count how many chars are left when the user is typing a message

--- scripts.js
+++ scripts.js
@@ -3,7 +3,15 @@ const app = new Vue(
     // el is the root component. Tell vue what class/id to look for
     el : '#app',
     data: {
-      tweetText: 'Hello bitch!'
+      tweetText: 'Hello mofo!',
+      charactersRemaining: 280,
+      commentText: '',
+      liked: false
+    },
+    methods: {
+      countCharacters: function(){
+        this.charactersRemaining = 280 - this.commentText.length;
+      }
     }
   }
 );
