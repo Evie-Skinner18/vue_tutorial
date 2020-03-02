85262570b96dd8949dfba73b5bddf4f99857eec3
Evie Skinner <evie.skinner@teamriver.com>
2020-03-02 15:03:02 +0000

    Changed the app so new tweets are in an array and a custom HTML element with a foreach renders a new component with dynamic tweet text going in via a param

---
+++ tweet.js
@@ -0,0 +1,71 @@
+
+Vue.component('tweet', {
+    props: ['tweetText', 'id'],
+    data: function() {
+      return {
+        charactersRemaining: 280,
+        commentText: '',
+        liked: false,
+        todaysDate: new Date().toLocaleDateString('es-ES')
+      }
+    },
+    methods: {
+      countCharacters: function() {
+        this.charactersRemaining = 280 - this.commentText.length
+      },
+      toggleLike: function() {
+        this.liked = !this.liked
+      }
+    },
+    // each tweet component in the tweets array will have this structure
+    template: `<!--to-do: remix this in your own way!-->
+    <!-- status div is the root component which links up with el in the js file -->
+    <div class="status">
+        <div class="tweet-content">
+        <img src="https://pbs.twimg.com/profile_images/1070775214370373633/borvu2Xx_400x400.jpg" class="logo" alt="Vue Vixens DC logo">
+        <div class="tweet">
+            <a href="https://twitter.com/vuevixensdc">Vue Vixens DC</a>
+            <span>@VueVixensDC · {{ todaysDate }}</span>
+            <!-- the value stored in the 'data' property of our root vue component will be rendered here-->
+            <p class="tweet-text">
+                {{ tweetText }}
+            </p>
+            <div class="reactions like" v-on:click="toggleLike">
+            <span class="like">
+                <span v-if="liked">♥️</span>
+                <span v-else>♡</span>
+            </span>
+            </div>
+        </div>
+        </div>
+        <div class="comment-bar">
+            <!-- v-model is a vue directive that will link up what the user types here to the var we've 
+            defined, in this case commentText -->
+        <textarea placeholder="tweet your reply" v-model="commentText" v-on:input="countCharacters"></textarea>
+        <span v-if="charactersRemaining <= 0" class="characters-remaining">
+            No more characters left soz!
+        </span>
+        <span v-else class="characters-remaining">
+            {{ charactersRemaining }} characters remaining
+        </span>
+        </div>
+    </div>`
+  })
+
+
+// el is the root component. Tell vue what class/id to look for
+const app = new Vue({ 
+    el : '#app',
+    data: {
+        tweets: [
+            {id: 1, tweetText: 'Hello world!'},
+            {id: 2, tweetText: 'Do ju wan to see my cucu??'},
+            {id: 3, tweetText: 'Bienvenidos a la primera aplicación Vue de Evie ;->'},
+            {id: 4, tweetText: 'Soy la hija del Duque de Cookies.'}
+        ]
+    }
+});
+
+
+  
+
