38a0033b4748215de6b27be268d9c741351cf2a8
Evie Skinner <evie.skinner@teamriver.com>
2020-03-02 13:56:19 +0000

    Added Vue conditional logic in the html tochange the heart that gets displayed on click

--- scripts.js
+++ scripts.js
@@ -1,20 +1,30 @@
-const app = new Vue(
-  {
-    // el is the root component. Tell vue what class/id to look for
-    el : '#app',
-    data: {
-      tweetText: 'Hello mofo!',
-      charactersRemaining: 280,
-      commentText: '',
-      liked: false
-    },
-    methods: {
-      countCharacters: function(){
-        this.charactersRemaining = 280 - this.commentText.length;
-      }
-    }
-  }
-);
+// const app = new Vue(
+//   {
+//     // el is the root component. Tell vue what class/id to look for
+//     el : '#app',
+//     data: {
+//       tweetText: 'Hola yo soy Paquita Salas!',
+//       charactersRemaining: 280,
+//       hasReachedCharacterLimit: false,
+//       commentText: '',
+//       liked: false
+//     },
+//     methods: {
+//       countCharacters: function(){
+//         this.charactersRemaining = 280 - this.commentText.length;
+//       },
+
+//       toggleLike: function(){
+//         this.liked = !this.liked;
+//       },
+
+//       checkCharacterLimit: function(){
+//         this.hasReachedCharacterLimit = (this.charactersRemaining === 0 ? true : false);
+//         console.log(this.hasReachedCharacterLimit);
+//       }
+//     }
+//   }
+// );
 
 
 
